90b47f2e2389f85c83de87aebfca046be910e6fe
Ankita0 <ankita.sharma2@mail.mcgill.ca>
2017-02-06 15:44:41 -0500

    Set up procedure blocks to fill

--- A3_caches/cache.vhd
+++ A3_caches/cache.vhd
@@ -1,3 +1,9 @@
+-- Group 
+-- Alina Mambo
+-- Ankita Sharma
+-- Maana Javadi
+-- Nicole Tang
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -30,9 +36,68 @@ end cache;
 architecture arch of cache is
 
 -- declare signals here
+constant c_bits_per_word: integer:= 32;
+constant c_word_per_block: integer:= 4;
+constant c_bits_per_block: integer:= 128;
+constant c_total_blocks: integer:= 32;
+
+
+type cache_state is (IDLE , C_TAG , C_DIRTY_BIT , R_MAIN_MEM , W_MAIN_MEM , W_CACHE);
+
+type chache_mem is array(31 downto 0) OF STD_LOGIC_VECTOR (127 downto 0);
+
+signal present_state: cache_state;
+signal next_state: cache_state;
+signal MISS, DIRTY_BIT : STD_LOGIC;
+
 
 begin
 
--- make circuits here
+procedure compare_tags is
+begin
+--TODO
+
+end compare_tags;
+
+procedure check_dirty_bits is
+begin
+--TODO
+
+end check_dirty_bits;
+
+procedure read_main_mem is
+begin
+--TODO
+
+end read_main_mem;
+
+procedure write_main_mem is
+begin
+--TODO
+
+end write_main_mem;
+
+
+procedure write_to_cache is
+begin
+--TODO
+
+end write_to_cache;
+
+procedure read_from_cache is
+begin
+--TODO
+
+end read_from_cache;
+
+
+cache_state_change: process (clock)
+begin
+--TODO
+
+
+end process;
+
+
 
 end arch;
