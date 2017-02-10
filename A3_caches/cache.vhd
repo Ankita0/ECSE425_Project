241f9b4537d6dbd9a9e7a0e47286c6360509c3c7
Ankita0 <ankita.sharma2@mail.mcgill.ca>
2017-02-10 17:17:25 -0500

    minor fix

--- A3_caches/cache.vhd
+++ A3_caches/cache.vhd
@@ -148,7 +148,7 @@ procedure write_to_cache_from_mm (signal mem_read_data_1 :in std_logic_vector(7 
 			signal mem_read_data_4 :in std_logic_vector(7 downto 0))is
 begin
 --TODO
-	s_write <= mem_read_data_4 & mem_read_data_3 & mem_read_data_2 & mem_read_data_1;
+	s_writedata <= mem_read_data_4 & mem_read_data_3 & mem_read_data_2 & mem_read_data_1;
 end write_to_cache_from_mm;
 
 begin
