97eee3395eda0e81dc364abe7520866be5e090a4
Ankita0 <ankita.sharma2@mail.mcgill.ca>
2017-02-10 01:53:51 -0500

    Fix 'Z' signal assignment in testbench. Contains '='assignment problem in testbench

--- A3_caches/cache_tb.vhd
+++ A3_caches/cache_tb.vhd
@@ -9,7 +9,7 @@ architecture behavior of cache_tb is
 
 component cache is
 generic(
-    ram_size : INTEGER := 32768;
+    ram_size : INTEGER := 32768
 );
 port(
     clock : in std_logic;
@@ -116,111 +116,47 @@ test_process : process
 begin
 
 --put test here
--- need to verify 'Z' signals
+-- need to verify 'X' signals
 -- filling in values for 
-wait for clk_period;
+	wait for clk_period;
 
-REPORT "start check for initialization of cache";
-s_read <= '0';
-s_write <= '0';
-s_addr <= 'Z';
-s_writedata <= 'Z';
-wait for clk_period;
-ASSERT((s_waitrequest= '0' and s_readdata = 'Z') and (m_addr = 'Z' and m_read = '0' and m_write = '0' and m_waitrequest = '0' and m_writedata= 'Z' and m_readdata = 'Z'))) 
-REPORT "INITIALIZATION unsuccessful"
-SEVERITY ERROR;
-REPORT "cache initialized";
+	REPORT "start check for initialization of cache";
+	s_read <= '0';
+	s_write <= '0';
+	s_addr <= "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+	s_writedata <= "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+	wait for clk_period;
+	ASSERT ((s_waitrequest= '0') and 
+		(s_readdata = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX") and 
+		(m_addr = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX") and
+		(m_read = '0')and
+		(m_write = '0')and
+		(m_waitrequest = '0') and
+		(m_writedata= "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX") and
+		(m_readdata = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"))
+	REPORT "INITIALIZATION unsuccessful" 
+	SEVERITY ERROR;
 
-REPORT "start check for read from cache once== IDLE read miss"
-s_read <= '1';
-s_write <= '0';
-s_addr <= '0000000000000000000000000000000';
-s_writedata <= 'Z';
-wait for clk_period/2;
-ASSERT (s_waitrequest= '1') 
-REPORT "(IDLE read miss) Busy state waitrequest not working" & integer'image(s_waitrequest) 
-SEVERITY ERROR;
-wait for clk_period/2;
-ASSERT (s_waitrequest= '1' and m_addr = '0000000000000000000000000000000' and m_read = '1' and m_write = '0' and m_waitrequest = '1') 
-REPORT "(IDLE read miss) main memory fetch request not working"
-SEVERITY ERROR;
-wait for clk_period/2;
-ASSERT (s_waitrequest= '1' and m_addr = '0000000000000000000000000000000' and m_read = '1' and m_write = '0' and m_waitrequest = '1')
-REPORT "IDLE read miss unsuccessful"
-SEVERITY ERROR;
-REPORT "IDLE read miss successful";
+	REPORT "cache initialized";
 
-REPORT "start  == "
-ASSERT () 
-REPORT " " & integer'image(s_op1) 
-SEVERITY ERROR;
-REPORT "";
-
-REPORT "start  == "
-ASSERT () 
-REPORT " " & integer'image(s_op1) 
-SEVERITY ERROR;
-REPORT "";
-
-REPORT "start  == "
-ASSERT () 
-REPORT " " & integer'image(s_op1) 
-SEVERITY ERROR;
-REPORT "";
-
-REPORT "start  == "
-ASSERT () 
-REPORT " " & integer'image(s_op1) 
-SEVERITY ERROR;
-REPORT "";
-
-REPORT "start  == "
-ASSERT () 
-REPORT " " & integer'image(s_op1) 
-SEVERITY ERROR;
-REPORT "";
-
-REPORT "start  == "
-ASSERT () 
-REPORT " " & integer'image(s_op1) 
-SEVERITY ERROR;
-REPORT "";
-
-REPORT "start  == "
-ASSERT () 
-REPORT " " & integer'image(s_op1) 
-SEVERITY ERROR;
-REPORT "";
-
-REPORT "start  == "
-ASSERT () 
-REPORT " " & integer'image(s_op1) 
-SEVERITY ERROR;
-REPORT "";
-
-REPORT "start  == "
-ASSERT () 
-REPORT " " & integer'image(s_op1) 
-SEVERITY ERROR;
-REPORT "";
-
-REPORT "start  == "
-ASSERT () 
-REPORT " " & integer'image(s_op1) 
-SEVERITY ERROR;
-REPORT "";
-
-REPORT "start  == "
-ASSERT () 
-REPORT " " & integer'image(s_op1) 
-SEVERITY ERROR;
-REPORT "";
-
-REPORT "start  == "
-ASSERT () 
-REPORT " " & integer'image(s_op1) 
-SEVERITY ERROR;
-REPORT "";
+	REPORT "start check for read from cache once== IDLE read miss";
+	s_read <= '1';
+	s_write <= '0';
+	s_addr <= "00000000000000000000000000000000";
+	s_writedata <= "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+	wait for clk_period/2;
+	ASSERT (s_waitrequest= '1') 
+	REPORT "(IDLE read miss) Busy state waitrequest not working" & std_logic'image(s_waitrequest) 
+	SEVERITY ERROR;
+	wait for clk_period/2;
+	ASSERT (s_waitrequest= '1' and m_addr = "00000000000000000000000000000000" and m_read = '1' and m_write = '0' and m_waitrequest = '1') 
+	REPORT "(IDLE read miss) main memory fetch request not working"
+	SEVERITY ERROR;
+	wait for clk_period/2;
+	ASSERT (s_waitrequest= '1' and m_addr = "00000000000000000000000000000000" and m_read = '1' and m_write = '0' and m_waitrequest = '1')
+	REPORT "IDLE read miss unsuccessful"
+	SEVERITY ERROR;
+	REPORT "IDLE read miss successful";
 
 end process;
 	
