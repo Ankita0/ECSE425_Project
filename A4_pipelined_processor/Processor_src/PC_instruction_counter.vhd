e84587b324b54b994e930232992435db968579aa
Ankita0 <ankita.sharma2@mail.mcgill.ca>
2017-03-02 14:54:17 -0500

    Set up PC counter initialize and normal behaviour

--- A4_pipelined_processor/Processor_src/PC_instruction_counter.vhd
+++ A4_pipelined_processor/Processor_src/PC_instruction_counter.vhd
@@ -7,11 +7,20 @@ ENTITY PC_instruction_counter IS
 
 PORT(
 PC_IN : IN std_logic_vector(31 downto 0);
-PC_OUT : OUT std_logic_vector(31 downto 0));
+INIT, CLK: IN std_logic;
+PC : OUT std_logic_vector(31 downto 0));
 
 END PC_instruction_counter;
 
-
 ARCHITECTURE PC_instruction_counter_behaviour IS
-
+SIGNAL ;
+BEGIN
+sync_PC: PROCESS(CLK, INIT)
+BEGIN
+	IF (rising_edge(INIT) and rising_edge(CLK)) THEN
+		PC<= "00000000000000000000000000000000";
+	ELSIF (rising_edge(CLK)) THEN
+		PC<= PC_IN;
+	END IF;
+END PROCESS sync_PC;
 END PC_instruction_counter_behaviour;
