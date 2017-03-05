a89a7c3536ad438968cd11ebb91e078072bc1aab
Ankita0 <ankita.sharma2@mail.mcgill.ca>
2017-03-05 00:57:20 -0500

    Fix compilation error due to input and output names

--- A4_pipelined_processor/Processor_src/signextension.vhd
+++ A4_pipelined_processor/Processor_src/signextension.vhd
@@ -3,13 +3,13 @@ use ieee.std_logic_1164.all;
 
 entity signextension is
 port(
-	16b_instruction : in std_logic_vector(15 downto 0);
-	32b_instruction : out std_logic_vector(31 downto 0)
+	bit16_instruction : in std_logic_vector(15 downto 0);
+	bit32_instruction : out std_logic_vector(31 downto 0)
 );
 end signextension;
 
 architecture arch of signextension is
 begin
-	32b_instruction <=	"0000000000000000" & 16b_instruction when 16b_instruction(15) ='0' else 
-						"1111111111111111" & 16b_instruction;
+	bit32_instruction <=	"0000000000000000" & bit16_instruction when bit16_instruction(15) ='0' else 
+						"1111111111111111" & bit16_instruction;
 end arch;
