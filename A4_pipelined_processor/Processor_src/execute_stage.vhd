1c1e3f4b81017695a226a861b9dab6a57770837e
amambo <alina.mambo@gmail.com>
2017-03-06 00:54:13 -0500

    Add zero branch block & execute testbed skeleton

---
+++ A4_pipelined_processor/Processor_src/execute_stage.vhd
@@ -0,0 +1,17 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity execute_stage is
+
+
+end execute_stage;
+
+
+architecture testbed of execute_stage is
+
+begin
+
+
+
+end testbed;
---
+++ A4_pipelined_processor/Processor_src/zero.vhd
@@ -0,0 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+ENTITY Zero is 
+
+PORT();
+
+end Zero;
+
+
+Architecture arch of zero is
+
+begin
+
+
+
+
+
+end arch;
