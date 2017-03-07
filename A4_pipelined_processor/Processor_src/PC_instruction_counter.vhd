0a4c6fe90181d85755d0d50cd372f4e1e318c055
Ankita0 <ankita.sharma2@mail.mcgill.ca>
2017-03-06 21:07:51 -0500

    Fix bugs in counter. Finish memory initilaization for instructions

--- A4_pipelined_processor/Processor_src/PC_instruction_counter.vhd
+++ A4_pipelined_processor/Processor_src/PC_instruction_counter.vhd
@@ -5,24 +5,25 @@ USE ieee.std_logic_signed.all;
 
 ENTITY PC_instruction_counter IS
 PORT(
-PC_IN : IN STD_LOGIC_VECTOR(31 downto 0);
-INIT, CLK: IN STD_LOGIC;
-PC : OUT STD_LOGIC_VECTOR(31 downto 0)
+	PC_IN : IN STD_LOGIC_VECTOR(31 downto 0);
+	INIT : IN STD_LOGIC;
+	CLK : IN STD_LOGIC;
+	PC : OUT STD_LOGIC_VECTOR(31 downto 0)
 );
 END PC_instruction_counter;
 
-ARCHITECTURE behaviour OF PC_instruction_counter IS
+ARCHITECTURE arch OF PC_instruction_counter IS
 BEGIN
 
 --Clock synchronized process for updating program counter component. With reset
 update_PC: PROCESS(CLK, INIT)
 BEGIN
 	--Reset to initialize PC
-	IF (rising_edge(INIT) AND rising_edge(CLK)) THEN
+	IF (rising_edge(INIT)) THEN
 		PC<= "00000000000000000000000000000000";
 	--Update PC instruction on clock rising edge
 	ELSIF (rising_edge(CLK)) THEN
 		PC<= PC_IN;
 	END IF;
 END PROCESS update_PC;
-END behaviour;
+END arch;
--- A4_pipelined_processor/Processor_src/instruction_memory.vhd
+++ A4_pipelined_processor/Processor_src/instruction_memory.vhd
@@ -1,9 +1,10 @@
 --Modified code from memory example given for cache assignment 3 to fit requirements
-
 --Adapted from Example 12-15 of Quartus Design and Synthesis handbook
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 USE ieee.numeric_std.all;
+USE ieee.std_logic_textio.all;
+USE STD.textio.all; 
 
 -------------------------------------------------------------------------------------
 --The instruction memory has 32 bit slots for one instruction
@@ -14,18 +15,17 @@ USE ieee.numeric_std.all;
 --Assuming that the file has to be written into memory at initilization for use, we: 
 --FIRST populate the memory bank
 --THEN read from it as needed
+--WRITING TO INSTURCTION MEMORY FUNCTIONALITY IS NOT AVAILABLE for our sanity :D
 -------------------------------------------------------------------------------------
 ENTITY instruction_memory IS
 	GENERIC(
-		ram_size : INTEGER := 32768;
+		ram_size : INTEGER := 1024;
 		mem_delay : time := 10 ns;
 		clock_period : time := 1 ns
 	);
 	PORT (
 		clock: IN STD_LOGIC;
-		writedata: IN STD_LOGIC_VECTOR (31 DOWNTO 0);
 		address: IN INTEGER RANGE 0 TO ram_size-1;
-		memwrite: IN STD_LOGIC;
 		memread: IN STD_LOGIC;
 		readdata: OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
 		waitrequest: OUT STD_LOGIC
@@ -38,37 +38,34 @@ ARCHITECTURE rtl OF instruction_memory IS
 	SIGNAL read_address_reg: INTEGER RANGE 0 to ram_size-1;
 	SIGNAL write_waitreq_reg: STD_LOGIC := '1';
 	SIGNAL read_waitreq_reg: STD_LOGIC := '1';
+	SIGNAL char_vector_to_store: std_logic_vector(31 downto 0) ;
 BEGIN
 	--This is the main section of the SRAM model
 	mem_process: PROCESS (clock)
+		file file_name:			text;
+		variable line_num:      	line;
+		variable char_vector_to_store:  std_logic_vector(31 downto 0);
 	BEGIN
 		--This is a cheap trick to initialize the SRAM in simulation
 		IF(now < 1 ps)THEN
+			--open file: path specified in the second argument
+			file_open (file_name, "C:\Users\Ankita\Documents\McGill\Winter2017\ECSE425_Project\A4_pipelined_processor\read.txt", READ_MODE);
+			--Read through 1024 lines of text file and save to memory			
 			For i in 0 to ram_size-1 LOOP
-				ram_block(i) <= std_logic_vector(to_unsigned(i,8));
+				readline (file_name, line_num); 
+				read (line_num, char_vector_to_store);
+				ram_block(i) <= char_vector_to_store;
 			END LOOP;
-		end if;
+		END IF;
 
 		--This is the actual synthesizable SRAM block
+		--Writing functionality taken away from instruction memory
 		IF (clock'event AND clock = '1') THEN
-			IF (memwrite = '1') THEN
-				ram_block(address) <= writedata;
-			END IF;
 		read_address_reg <= address;
 		END IF;
 	END PROCESS;
 	readdata <= ram_block(read_address_reg);
 
-
-	--The waitrequest signal is used to vary response time in simulation
-	--Read and write should never happen at the same time.
-	waitreq_w_proc: PROCESS (memwrite)
-	BEGIN
-		IF(memwrite'event AND memwrite = '1')THEN
-			write_waitreq_reg <= '0' after mem_delay, '1' after mem_delay + clock_period;
-
-		END IF;
-	END PROCESS;
 
 	waitreq_r_proc: PROCESS (memread)
 	BEGIN
