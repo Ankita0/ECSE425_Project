448966093f0a154a74166ad8bfa86a39551acbd0
amambo <alina.mambo@gmail.com>
2017-03-14 01:31:05 -0400

    Test 1 ALU success and complete

--- A4_pipelined_processor/Processor_src/alu.vhd
+++ A4_pipelined_processor/Processor_src/alu.vhd
@@ -15,11 +15,11 @@ entity alu is
 
 	Generic(W : natural := 32; F : natural :=6;clock_period : time := 1 ns);
 	
-	port( 	Mux_A	: in  std_logic_vector(W-1 downto 0);
+	port(Mux_A	: in  std_logic_vector(W-1 downto 0);
 			Mux_B	: in  std_logic_vector(W-1 downto 0);
 			Alu_Ctrl: in  std_logic_vector(F-1 downto 0);
-			Shift	: in std_logic_vector (F-2 downto 0);
-			clock 	: in std_logic;
+			clock : in std_logic;
+			shamt	: in std_logic_vector (F-2 downto 0);
 			Alu_Rslt: out std_logic_vector(W-1 downto 0);
 			Zero 	: out std_logic;
 			Overflow: out std_logic;
@@ -37,35 +37,38 @@ architecture arch of alu is
 
 		process(Alu_Ctrl)
 
-		Variable Y := std_logic_vector(W-1 downto 0);
-		Variable sign_A,sign_B := signed(W-1 downto 0);
-		variable MD_rslt := std_logic_vector(63 downto 0);
-		variable shift_zeroes:= (others => '0');
-		variable shamt := to_integer(signed(shamt));
+		Variable Y : std_logic_vector(W-1 downto 0);
+		Variable sign_A,sign_B : signed(W-1 downto 0);
+		Variable alu_c : std_logic_vector(5 downto 0);
+		variable MD_rslt : std_logic_vector(63 downto 0);
+		variable shift_zeroes: std_logic_vector(W-1 downto 0);
+		
 
 		begin
+		  alu_c := Alu_Ctrl;
+		  shift_zeroes := "00000000000000000000000000000000";
 
-			case Alu_Ctrl is
+			case alu_c is
 
 				--R-Type
-				when "100000"=> Y 	:= 	signed(Mux_A) + signed(Mux_B); --ADD
-				when "100010"=>	Y	:= 	signed(Mux_A) - signed(Mux_B); --SUB
+				when "100000"=> Y 	:= 	std_logic_vector(signed(Mux_A) + signed(Mux_B)); --ADD
+				when "100010"=>	Y	:= 	std_logic_vector(signed(Mux_A) - signed(Mux_B)); --SUB
 				when "100100"=> Y 	:= 	Mux_A AND Mux_B; --AND
 				when "100101"=> Y	:= 	Mux_A OR Mux_B;-- OR
 				when "100111"=>	Y	:= 	Mux_A OR NOT Mux_B; --NOR
 				when "100110"=> Y 	:=	(Mux_A AND (NOT Mux_B)) OR ((NOT Mux_A) AND Mux_B); --XOR
 
 				when "011000"=>	
-					MD_rslt:= signed(Mux_A)*signed(Mux_B);--Mult
+					MD_rslt:= std_logic_vector(signed(Mux_A)*signed(Mux_B));--Mult
 					Lo<=MD_rslt(31 downto 0);
 					Hi<= MD_rslt(63 downto 32);
 				
 				when "011010"=>		--Div
-					Lo<= signed(Mux_A)/signed(Mux_B);
-					Hi<= signed(Mux_A)rem signed(Mux_B);
+					Lo<= std_logic_vector(signed(Mux_A)/signed(Mux_B));
+					Hi<= std_logic_vector(signed(Mux_A)rem signed(Mux_B));
 
 				when "101010"=>
-					IF(Mux_A<Mux_B) then --set on less than (slt)
+					IF(signed(Mux_A)<signed(Mux_B)) then --set on less than (slt)
 						Y:="00000000000000000000000000000001";
 					else
 						Y:="00000000000000000000000000000000";
@@ -74,17 +77,17 @@ architecture arch of alu is
 
 				-- IF this does not work we need to save the part of the input we cant and & zeroes to it
 
-				when "000000"=> Y:= Mux_A shift_left shamt;	--shift left logical (sll), shift zeroes into LSB
-				when "000010"=>	Y:= Mux_A shift_right shamt;--shift right logical (srl), shift zeroes into MSB
-				when "000011"=>	--shift right arithmetic (sra)
-					IF(Mux_A(31)='1')then
-						Y:= Mux_A(shamt downto 0) & others => '1';
-					else
-						Y:= Mux_A(shamt downto 0) & others => '0';
-					END IF;
+				when "000000"=> Y:= std_logic_vector(unsigned(Mux_A) sll (to_integer(unsigned(shamt))) );	--shift left logical (sll), shift zeroes into LSB
+				when "000010"=>	Y:= std_logic_vector(unsigned(Mux_A) srl (to_integer(unsigned(shamt))) );--shift right logical (srl), shift zeroes into MSB
+				--when "000011"=>	--shift right arithmetic (sra) NEED TO FIX THE CONCAT
+				--	IF(signed(Mux_A(31))='1')then
+				--   Y:= Mux_A((to_integer(signed(shamt))) downto 0) & others <= '1';
+					--else
+					--	Y:= Mux_A((to_integer(signed(shamt))) downto 0) & others <= '0';
+					--END IF;
 
 				when "010000"=>	Y	:=Hi;--mfhi
-				when "100101"=>	Y	:=Lo;--mflo
+				--when "100101"=>	Y	:=Lo;--mflo duplicate code of OR
 
 				-- I type 
 				-- Note: andi/addi/ori/xori are the same logic as add/and/or/xor R type
@@ -97,34 +100,32 @@ architecture arch of alu is
 						Y:="00000000000000000000000000000000";
 					END IF;
 
-				when "001000"=>	Y	:= signed(Mux_A)+ signed(Mux_B); --addi,SW,LW
+				when "001000"=>	Y	:= std_logic_vector(signed(Mux_A)+ signed(Mux_B)); --addi,SW,LW
 				when "001100"=>	Y	:= Mux_A AND Mux_B;--andi
 				when "001101"=>	Y	:= Mux_A OR Mux_B;--ori
 				when "001110"=>	Y	:= (Mux_A AND (NOT Mux_B)) OR ((NOT Mux_A) AND Mux_B);--xori
 
-				when "000100"=>	--beq
-					IF(Mux_A=Mux_B) then 
+				--when "000100"=>	--beq
+				--	IF(Mux_A=Mux_B) then 
 						Zero<= '0';
-					END IF;
-				when "000101"=>	Y	:=;--bne
+				--	END IF;
+				--when "000101"=>	Y	:=;--bne
 
 				--LUI
 
 				--J-type
-				when "000011"=>	Y	:=;--jal
+				--when "000011"=>	Y	:=;--jal
 				--when "" => Y :=; --j
 				--when "" => Y :=; --jr
 
-
-
-				when others => Y (others => 'X');
+				when others => Y := (others => 'X');
 
 			end case;
 
 
 			--Check Zero
 
-			if Y ='0' then
+			if Y ="00000000000000000000000000000000" then
 				Zero<='1';
 			else
 				Zero <='0';
--- A4_pipelined_processor/Processor_src/alu_tb.vhd
+++ A4_pipelined_processor/Processor_src/alu_tb.vhd
@@ -3,21 +3,21 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity alu_testbed is
-
-
 end alu_testbed;
 
 architecture arch of alu_testbed is
 
 	Component alu is
 
-	Generic(W : natural := 32; F : natural :=6;clock_period : time := 1 ns);
+	Generic( W : natural := 32; 
+	         F : natural :=6;
+	         clock_period : time := 1 ns);
 	
 	port( 	Mux_A	: in  std_logic_vector(W-1 downto 0);
 			Mux_B	: in  std_logic_vector(W-1 downto 0);
 			Alu_Ctrl: in  std_logic_vector(F-1 downto 0);
-			Shift	: in std_logic_vector (F-2 downto 0);
-			clock 	: in std_logic;
+			shamt	: in std_logic_vector (F-2 downto 0);
+			clock : in std_logic;
 			Alu_Rslt: out std_logic_vector(W-1 downto 0);
 			Zero 	: out std_logic;
 			Overflow: out std_logic;
@@ -25,36 +25,59 @@ architecture arch of alu_testbed is
 	end Component;
 
 	constant clk_period : time := 1 ns;
-	signal clock : std_logic := '0';
-	signal Mux_A, Mux_B : std_logic_vector(W-1 downto 0);
-	signal Alu_Ctrl : std_logic_vector(W-1 downto 0);
-	signal Shift : std_logic_vector(F-1 downto 0);
-	signal Alu_Rslt : std_logic_vector(W-1 downto 0);
-	signal Zero : out std_logic;
-	signal Overflow : out std_logic;
-	signal Carryout : out std_logic;
+	signal Mux_A, Mux_B : std_logic_vector(31 downto 0) :=(others=>'0');
+	signal Alu_Ctrl : std_logic_vector(5 downto 0):=(others=>'0');
+	signal shamt : std_logic_vector(4 downto 0):=(others=>'0');
+	signal clk : std_logic := '0';
+	signal Alu_Rslt : std_logic_vector(31 downto 0):=(others=>'0');
+	signal Zero : std_logic := '0';
+	signal Overflow : std_logic := '0';
+	signal Carryout : std_logic := '0';
 
 	BEGIN
 
-		dut: alu
-			PORT_MAP(
+		dut: alu 
+			PORT MAP(
 				Mux_A,
 				Mux_B,
 				Alu_Ctrl,
-				Shift,
-				clock,
+				shamt,
+				clk,
 				Alu_Rslt,
 				Zero,	
 				Overflow,
 				Carryout);
+				
+		clk_process: process
+		  begin
+		    clk <='0';
+		    wait for clk_period/2;
+		    clk<='1';
+		    wait for clk_period/2;
+		   end process;
+		   
 
-		process
+		test_process: process
 
 			BEGIN
 
+    			--wait for 10 ns;
+    			
+    			--TEST and
+    			Alu_Ctrl<="100010";
+    			Mux_A <= x"FF22000F";
+    			Mux_B<= x"0542C0F8";
     			wait for clk_period;
+    			ASSERT (Alu_Rslt = x"F9DF3F17") REPORT "SUB works!!" SEVERITY ERROR;
+    			
+    			
+			 Alu_Ctrl<="100100";
+    			Mux_A <= x"FF22000F";
+    			Mux_B<= x"0542C0F8";
+    			wait for clk_period;
+    			ASSERT (Alu_Rslt = x"05020008") REPORT "ANDING works!!" SEVERITY ERROR;
 
 
-
+  end process;
 
 end arch;
