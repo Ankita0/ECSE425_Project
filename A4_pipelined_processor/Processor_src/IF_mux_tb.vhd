360b384fd18dc5b0a634a32736f7f5feefadee47
Ankita0 <ankita.sharma2@mail.mcgill.ca>
2017-03-16 02:12:24 -0400

    Remove clock dependancy + all unit level test for IF stage

--- A4_pipelined_processor/Processor_src/IF_mux_tb.vhd
+++ A4_pipelined_processor/Processor_src/IF_mux_tb.vhd
@@ -10,7 +10,6 @@ ARCHITECTURE behav of IF_mux_tb is
 		PC_instr_from_EX: IN INTEGER;
 		PC_instr_plus4: IN INTEGER;
 		mux_control: IN STD_LOGIC; 
-		CLK: IN STD_LOGIC;
 		PC_instr_to_fetch: OUT INTEGER
 	);
 	END COMPONENT;
@@ -19,7 +18,6 @@ ARCHITECTURE behav of IF_mux_tb is
 	SIGNAL PC_instr_from_EX: INTEGER:= 0;
 	SIGNAL PC_instr_plus4: INTEGER := 0;
 	SIGNAL mux_control: STD_LOGIC; 
-	SIGNAL CLK: STD_LOGIC;
 	SIGNAL PC_instr_to_fetch: INTEGER;
 
 BEGIN
@@ -29,32 +27,25 @@ DUT:
         PORT MAP(
 	        PC_instr_from_EX,
 		PC_instr_plus4,
-		mux_control, 
-		CLK,
+		mux_control,
 		PC_instr_to_fetch
 	);
-
-clk_process : process
-    BEGIN
-        clk <= '0';
-        wait for clk_period/2;
-        clk <= '1';
-        wait for clk_period/2;
-    END process;
 
 test_process : process
     BEGIN
         PC_instr_from_EX<=0;
 	PC_instr_plus4<=4;
 	mux_control <= '0';
-        WAIT for 1*clk_period;
+        WAIT for 0.5*clk_period;
         ASSERT PC_instr_to_fetch = 4 REPORT "muxing unsuccessful" SEVERITY error;
-        mux_control <= '1';
-	WAIT for 1*clk_period;
+        WAIT for 0.5*clk_period;
+	mux_control <= '1';
+	WAIT for 0.5*clk_period;
 	ASSERT PC_instr_to_fetch = 0 REPORT "muxing unsuccessful" SEVERITY error;
+	WAIT for 0.5*clk_period;
 	PC_instr_from_EX<=2;
 	PC_instr_plus4<=3;
-	WAIT for 1*clk_period;
+	WAIT for 0.5*clk_period;
 	ASSERT PC_instr_to_fetch = 2 REPORT "muxing unsuccessful" SEVERITY error;
         WAIT;
     END process;
