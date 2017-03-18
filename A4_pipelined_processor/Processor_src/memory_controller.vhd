9774f2e15e4ecc0b5eff1d9a1de2c1979d9ba0e4
mjava <maanajavadi@gmail.com>
2017-03-18 13:37:31 -0400

    Updated memory controller and testbench

--- A4_pipelined_processor/Processor_src/memory_controller.vhd
+++ A4_pipelined_processor/Processor_src/memory_controller.vhd
@@ -14,16 +14,17 @@ PORT(
   		  do_memwrite: IN STD_LOGIC;
       
       --coming from EX stage
-		  alu_data: IN STD_LOGIC_VECTOR (31 DOWNTO 0);
+		  alu_result: IN STD_LOGIC_VECTOR (31 DOWNTO 0);
+		  writedata: IN STD_LOGIC_VECTOR (31 DOWNTO 0);
 		  reg_dst: IN STD_LOGIC_VECTOR (4 DOWNTO 0);
 		  reg_write: IN STD_LOGIC;
-  		  address: IN INTEGER RANGE 0 TO ram_size-1; --value of read/write address
+  		  --address: IN INTEGER RANGE 0 TO ram_size-1; --value of read/write address
 
       --going to WB stage
-  		  data_WB: OUT STD_LOGIC_VECTOR (31 DOWNTO 0); --from ALU/memory
+  		  data_to_WB: OUT STD_LOGIC_VECTOR (31 DOWNTO 0); --from ALU/memory
   		  reg_dst_out: OUT STD_LOGIC_VECTOR (4 DOWNTO 0);
-  		  reg_write_out: OUT STD_LOGIC;
-  		  address_out: OUT INTEGER RANGE 0 TO ram_size-1
+  		  reg_write_out: OUT STD_LOGIC
+  		  --address_out: OUT INTEGER RANGE 0 TO ram_size-1
 		);
 END memory_controller;
 
@@ -48,52 +49,55 @@ END COMPONENT;
 
 	SIGNAL data_out: STD_LOGIC_VECTOR (31 DOWNTO 0);
 	--SIGNAL clk: STD_LOGIC;  
- 	SIGNAL mm_address: INTEGER RANGE 0 to ram_size-1;
- 	SIGNAL data_memread: STD_LOGIC_VECTOR (31 DOWNTO 0);
  	SIGNAL data_memwrite: STD_LOGIC_VECTOR (31 DOWNTO 0);
- 	SIGNAL mm_read: STD_LOGIC;
+ 	SIGNAL mm_address: INTEGER RANGE 0 to ram_size-1;
  	SIGNAL mm_write: STD_LOGIC;
- 	SIGNAL waitrequest: STD_LOGIC; 
+ 	SIGNAL mm_read: STD_LOGIC;
+ 	SIGNAL data_memread: STD_LOGIC_VECTOR (31 DOWNTO 0);
+	SIGNAL waitrequest: STD_LOGIC; 
   
 BEGIN
-data_out <= data_memread when do_memread = '1' else alu_data;
+  
+  data_out <= data_memread when do_memread = '1' else alu_result;
 
-DUT: data_memory
+  DUT: data_memory
       port map(
-	clock,
-	data_memwrite,
-	mm_address,
-	mm_write,
-	mm_read,
-	data_memread,
-	waitrequest
-);
+	     clock,
+      	data_memwrite,
+      	mm_address,
+      	mm_write,
+      	mm_read,
+       data_memread,
+      	waitrequest
+      );
 
-mm_address<=address;
-MEM_PROCESS : PROCESS  
-BEGIN
-	if rising_edge(clock) then
+  mm_address <= to_integer(unsigned(alu_result));
+  
+  MEM_PROCESS : PROCESS  
+  BEGIN
+    
+	 if rising_edge(clock) then
 		if do_memread = '1' then
 			mm_read <= '1';
 		elsif do_memwrite = '1' then
 			mm_write <= '1';
 		end if;
 		--wait until rising_edge(waitrequest);
-	end if; 
-end process;
+	 end if; 
+  end process;
 	
-MEMSTAGE_PROCESS : PROCESS (clock, reset)	
-	BEGIN
+  MEMSTAGE_PROCESS : PROCESS (clock, reset)	
+	 BEGIN
 	  
 	  if reset = '1' then
 	    
-	    data_WB <= (others => '0');
+	    data_to_WB <= (others => '0');
 	    reg_dst_out <= (others => '0');
 	    
 	  elsif rising_edge(clock) then
 	    
-	    data_WB <= data_out;
-	    address_out <= address;
+	    data_to_WB <= data_out;
+	    --address_out <= address;
 	    reg_dst_out <= reg_dst;
 	    reg_write_out <= reg_write;
 
--- A4_pipelined_processor/Processor_src/memory_controller_tb.vhd
+++ A4_pipelined_processor/Processor_src/memory_controller_tb.vhd
@@ -9,7 +9,9 @@ ARCHITECTURE behaviour OF memory_controller_tb IS
 
 --Declare the component that you are testing:
     COMPONENT memory_controller IS
-
+      GENERIC(
+      	ram_size : INTEGER := 8192
+      );
       PORT(
       clock, reset: IN STD_LOGIC;
       
@@ -18,16 +20,17 @@ ARCHITECTURE behaviour OF memory_controller_tb IS
   		  do_memwrite: IN STD_LOGIC;
       
       --coming from EX stage
-		  alu_data: IN STD_LOGIC_VECTOR (31 DOWNTO 0);
+		  alu_result: IN STD_LOGIC_VECTOR (31 DOWNTO 0);
+		  writedata: IN STD_LOGIC_VECTOR (31 DOWNTO 0);
 		  reg_dst: IN STD_LOGIC_VECTOR (4 DOWNTO 0);
 		  reg_write: IN STD_LOGIC;
-  		  address: IN INTEGER RANGE 0 TO ram_size-1; --value of read/write address
+  		  --address: IN INTEGER RANGE 0 TO ram_size-1; --value of read/write address
 
       --going to WB stage
-  		  data_WB: OUT STD_LOGIC_VECTOR (31 DOWNTO 0); --from ALU/memory
+  		  data_to_WB: OUT STD_LOGIC_VECTOR (31 DOWNTO 0); --from ALU/memory
   		  reg_dst_out: OUT STD_LOGIC_VECTOR (4 DOWNTO 0);
-  		  reg_write_out: OUT STD_LOGIC;
-  		  address_out: OUT INTEGER RANGE 0 TO ram_size-1
+  		  reg_write_out: OUT STD_LOGIC
+  		  --address_out: OUT INTEGER RANGE 0 TO ram_size-1
 		);
     END COMPONENT;
 
@@ -35,16 +38,17 @@ ARCHITECTURE behaviour OF memory_controller_tb IS
     signal clock : std_logic := '0';
     signal reset : std_logic := '0';
     constant clk_period : time := 1 ns;
-    signal address: INTEGER RANGE 0 TO 8192-1;
+    --signal address: INTEGER RANGE 0 TO 8192-1;
     signal memwrite: STD_LOGIC := '0';
     signal memread: STD_LOGIC := '0';
-    signal alu_data: STD_LOGIC_VECTOR (31 DOWNTO 0) := (others=> '0');
-    signal reg_dst: STD_LOGIC_VECTOR (4 DOWNTO 0) := (others=> '0');
-    signal reg_write: STD_LOGIC := '0';
-    signal data_WB: STD_LOGIC_VECTOR (31 DOWNTO 0) := (others=> '0');
-    signal reg_dst_out: STD_LOGIC_VECTOR (4 DOWNTO 0) := (others=> '0');
-    signal reg_write_out: STD_LOGIC := '0';
-    signal address_out: INTEGER RANGE 0 TO 8192-1;
+    signal alu_result: STD_LOGIC_VECTOR (31 DOWNTO 0);
+    signal reg_dst: STD_LOGIC_VECTOR (4 DOWNTO 0);
+    signal reg_write: STD_LOGIC;
+    signal data_to_WB: STD_LOGIC_VECTOR (31 DOWNTO 0);
+    signal writedata: STD_LOGIC_VECTOR (31 DOWNTO 0);
+    signal reg_dst_out: STD_LOGIC_VECTOR (4 DOWNTO 0);
+    signal reg_write_out: STD_LOGIC ;
+    --signal address_out: INTEGER RANGE 0 TO 8192-1;
 
 BEGIN
 
@@ -55,14 +59,15 @@ BEGIN
                     reset => reset,
                     do_memread => memread,
                     do_memwrite => memwrite,
-                    alu_data => alu_data,
+                    alu_result => alu_result,
                     reg_dst => reg_dst,
                     reg_write => reg_write,
-                    address => address,
-                    data_WB => data_WB,
+                    --address => address,
+                    data_to_WB => data_to_WB,
+                    writedata => writedata,
                     reg_dst_out => reg_dst_out,
-                    reg_write_out => reg_write_out,
-                    address_out => address_out
+                    reg_write_out => reg_write_out
+                    --address_out => address_out
                 );
 
     clk_process : process
@@ -76,17 +81,27 @@ BEGIN
     test_process : process
     BEGIN
 
+        wait for clk_period;      
+ 
+        alu_result <= x"00000001";
+        writedata <= x"00000002";
+
+        --wait until rising_edge(waitrequest);
         memread <= '0';
-        memwrite <= '1'; 
-        alu_data <= "00000000000000000000000000000011";
-        wait for clk_period;
-        ASSERT (data_WB = "00000000000000000000000000000011") REPORT "NOT WRITTEN!" SEVERITY ERROR;
-        
-        memread <= '1';
+        memwrite <= '1';
+        --wait until rising_edge(waitrequest);
+        ASSERT data_to_WB = x"00000001" REPORT "alu_result unsuccessful pass" SEVERITY ERROR;
         memwrite <= '0';
-        alu_data <= "00000000000000000000000000000010";
         wait for clk_period;
-        ASSERT (data_WB = "00000000000000000000000000000011") REPORT "NOT READ!" SEVERITY ERROR;
+        memread <= '1';
+        --wait until rising_edge(waitrequest);
+        assert data_to_WB = x"00000002" report "unsuccessful read from memory" severity error;
+        memread <= '0';
+        wait;
+        --wait for clk_period;
+        --memread <= '1';
+        --wait until rising_edge(waitrequest);
+        --ASSERT data_to_WB = x"00000000000000000000000000000011") REPORT "NOT READ!" SEVERITY ERROR;
 
     END PROCESS;
 
