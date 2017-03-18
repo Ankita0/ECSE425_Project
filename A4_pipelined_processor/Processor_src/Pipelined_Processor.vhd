4e9df7c4dc0748fd761aa85cca12e02f4d8eb266
Ankita0 <ankita.sharma2@mail.mcgill.ca>
2017-03-18 00:58:06 -0400

    Add processor instantiation

---
+++ A4_pipelined_processor/Processor_src/Pipelined_Processor.vhd
@@ -0,0 +1,16 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.numeric_std.all;
+USE ieee.std_logic_textio.all;
+USE STD.textio.all; 
+
+
+entity Pipelined_Processor is
+PP_Init: IN STD_LOGIC;
+PP_CLK: IN STD_LOGIC;
+PP_data: OUT STD_LOGIC_VECTOR(31 downto 0);
+end Pipelined_Processor;
+
+ARCHITECTURE foo of Pipelined_Processor is
+
+END ARCHITECTURE;
