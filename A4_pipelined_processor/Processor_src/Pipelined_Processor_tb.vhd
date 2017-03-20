446609c856380a1210745c307d007d0e1039c695
Alina Mambo <alina.mambo@gmail.com>
2017-03-19 23:24:43 -0400

    something works in testbench

--- A4_pipelined_processor/Processor_src/Pipelined_Processor_tb.vhd
+++ A4_pipelined_processor/Processor_src/Pipelined_Processor_tb.vhd
@@ -15,15 +15,20 @@ PORT(
 	PP_CLK: IN STD_LOGIC;
 	POOP: IN STD_LOGIC;
 	PP_reg_data: OUT STD_LOGIC_VECTOR(31 downto 0);
-	PP_mm_data: OUT STD_LOGIC_VECTOR(31 downto 0)
+	PP_reg_number:OUT std_logic_vector(4 downto 0);-- data type can be changed
+	PP_mm_data: OUT STD_LOGIC_VECTOR(31 downto 0);
+	PP_mm_addr: OUT INTEGER-- data type can be changed
 );
 END COMPONENT;
-      
+    
+  constant clk_period: time := 1 ns;  
 	SIGNAL PP_Init: STD_LOGIC:='0';
 	SIGNAL PP_CLK: STD_LOGIC:='0';
 	SIGNAL POOP: STD_LOGIC:='0';
 	SIGNAL PP_reg_data: STD_LOGIC_VECTOR(31 downto 0):= (others => '0');
 	SIGNAL PP_mm_data: STD_LOGIC_VECTOR(31 downto 0):= (others => '0');
+	Signal PP_reg_number : std_logic_vector(4 downto 0):= (others => '0');
+	signal PP_mm_addr: integer := 0;
 
 BEGIN
 
@@ -33,21 +38,27 @@ Pipelined_Processor PORT MAP(
 	PP_CLK,
 	POOP,
 	PP_reg_data,
-	PP_mm_data
-
+	PP_reg_number,
+	PP_mm_data,
+	PP_mm_addr
 );
 	clk_process: process
 	 begin
-		    clk <='0';
+		    PP_clk <='0';
 		    wait for clk_period/2;
-		    clk<='1';
+		    PP_clk<='1';
 		    wait for clk_period/2;
 
 	end process;
 
-test_process: PROCESS (clk)
+test_process: PROCESS 
 
 BEGIN
+  
+  PP_INIT<='0';
+  
+  wait for clk_period;
+  
 
 END PROCESS;
 
