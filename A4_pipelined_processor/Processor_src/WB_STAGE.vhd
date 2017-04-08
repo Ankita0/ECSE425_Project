cce936a78a5cf3050b326c8ef46cd857baf7adb5
Ankita Sharma <ankita.sharma2@mail.mcgill.ca>
2017-04-07 23:10:13 -0400

    Branch merge: moved mux input signals to not be clk dependent

--- A4_pipelined_processor/Processor_src/WB_STAGE.vhd
+++ A4_pipelined_processor/Processor_src/WB_STAGE.vhd
@@ -31,17 +31,16 @@ ARCHITECTURE behaviour of WB_STAGE is
     
   BEGIN
     MUX: EX_MUX PORT MAP(alu_data_s, readdata, reg_write, clock, mux_data);
-      
     write_back : process(clock)
     BEGIN
-      if rising_edge(clock) then
-        readdata <= mem_data;
+	readdata <= mem_data;
         alu_data_s <= alu_data;
         reg_write_s <= reg_write;
-        writedata <= mux_data;
-        reg_dst_out <= reg_dst;
-        reg_write_out <= reg_write;
-    end if;
+
+	if rising_edge(clock) then
+		reg_write_out <= reg_write;
+        	reg_dst_out <= reg_dst;
+		writedata <= mux_data;
+	end if;
   end process;
 end behaviour;
-        
